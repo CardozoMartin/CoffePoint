12dc2347febc2398579879b826b99fc613ec273f
martin cardozo <tiin.-@hotmail.es>
2025-06-12 11:52:50 -0300

    se agrega comentarios para el flujo del codigo

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -12,14 +12,19 @@ export class auth {
         const { email, contrasenia } = req.body;
 
         try {
+            //primero buscamos un cliente por el id
             const buscarClienteEnBD = await Cliente.findOne({ email: email })
+
+            //luego verficamos que el cliente tenga el mismo email y la misma contraseña
             if (!buscarClienteEnBD || !bcrypt.compareSync(contrasenia, buscarClienteEnBD.contrasenia)) {
                 res.status(404).json({ message: "Usuario o Contraseña incorrectos", data: null })
                 return;
             }
 
             //si esta todo okey
-
+            //este es el modelo de datos que nosotros mandamos al front
+            //con esto podemos obtener la infomacion del usaurio para verficiar su nombre y saber
+            //a que cosas puede o no puede acceder en el front
             const datosDelUsuario = {
                 cliente: {
                     id: buscarClienteEnBD?._id,
@@ -30,6 +35,9 @@ export class auth {
                     telefono: buscarClienteEnBD?.telefono
                 }
             }
+
+            //esta es el toke de accesso aqui enviamos la info y la clave secreta la usamos para crear el toke
+            //ponemos que cada 1hora el token se eliminara y debera volvera iniciar session
             const token = jwt.sign(datosDelUsuario, JWT_SECRET_KEY, {
                 expiresIn: '1h',
             });
